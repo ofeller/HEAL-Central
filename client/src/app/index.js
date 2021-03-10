f4c3a7d0c843dcd12fb1871307c53736dcb19a06
katterillo <kattrina.erillo@gmail.com>
2021-03-10 16:27:35 -0500

    set up basic NavBar

---
+++ client/src/app/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { NavBar } from '../components'
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+
+import logo from '../heal_logo.png'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <Link to='/'>
+                    <img src={logo} class="m-auto w-48 h-48"/>
+                    </Link>
+                <div class="flex justify-around">
+                    <Link to='/'> 
+                        <h1 class=""> HOME </h1>
+                    </Link>
+                    <Link to='/'> 
+                        <h1 class=""> ABOUT </h1>
+                    </Link>
+                    <Link to='/'> 
+                        <h1 class=""> CALENDAR </h1>
+                    </Link>
+                    <Link to='/'> 
+                        <h1 class=""> GALLERY </h1>
+                    </Link>
+                    <Link to='/'> 
+                        <h1 class=""> CONTACT </h1>
+                    </Link>
+                    <div class="flex">
+                        <Link to='/'> 
+                            <h1 class=""> SOCIAL#1 </h1>
+                        </Link>
+                        <Link to='/'> 
+                            <h1 class=""> SOCIAL#2 </h1>
+                        </Link>
+                        <Link to='/'> 
+                            <h1 class=""> SOCIAL#3 </h1>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBar
