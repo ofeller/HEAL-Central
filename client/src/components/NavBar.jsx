e6a564c10549a9c52c0a1e5de31a11d368645130
katterillo <kattrina.erillo@gmail.com>
2021-03-13 00:28:00 -0500

    made navbar fixed and removed other styling

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,40 +1,40 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom';
 
-import logo from '../heal_logo.png'
+import logo from '../heal_logo.PNG'
 
 class NavBar extends Component {
     render() {
         return (
-            <div class="bg-white">
-                <Link to='/'>
-                    <img src={logo} class="m-auto w-48 h-48 fixed inset-x-0"/>
+            <div class="fixed bg-white m-auto w-full">
+                <div class="flex flex-wrap justify-around items-center">
+                    <Link to='/'>
+                        <img src={logo} class="m-auto w-20 h-20"/>
                     </Link>
-                <div class="flex justify-around">
                     <Link to='/'> 
-                        <h1 class="mt-40 fixed"> HOME </h1>
+                        <h1 class=""> HOME </h1>
                     </Link>
                     <Link to='/about'> 
-                        <h1 class="mt-40 fixed"> ABOUT </h1>
+                        <h1 class=""> ABOUT </h1>
                     </Link>
                     <Link to='/calendar'> 
-                        <h1 class="mt-40 fixed"> CALENDAR </h1>
+                        <h1 class=""> CALENDAR </h1>
                     </Link>
                     <Link to='/gallery'> 
-                        <h1 class="mt-40 fixed"> GALLERY </h1>
+                        <h1 class=""> GALLERY </h1>
                     </Link>
                     <Link to='/'> 
-                        <h1 class="mt-40 fixed"> CONTACT </h1>
+                        <h1 class=""> CONTACT </h1>
                     </Link>
                     <div class="flex">
                         <Link to='/'> 
-                            <h1 class="mt-40 fixed"> SOCIAL#1 </h1>
+                            <h1 class=""> SOCIAL#1 </h1>
                         </Link>
                         <Link to='/'> 
-                            <h1 class="mt-40 fixed"> SOCIAL#2 </h1>
+                            <h1 class=""> SOCIAL#2 </h1>
                         </Link>
                         <Link to='/'> 
-                            <h1 class="mt-40 fixed"> SOCIAL#3 </h1>
+                            <h1 class=""> SOCIAL#3 </h1>
                         </Link>
                     </div>
                 </div>
