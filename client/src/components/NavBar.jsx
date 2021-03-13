75e3853cea4f729f97d538724622495714768d2c
Olivia Feller <oliviafeller@Olivias-MacBook-Pro-2.local>
2021-03-12 23:31:19 -0500

    Sticky nav bar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,35 +6,35 @@ import logo from '../heal_logo.png'
 class NavBar extends Component {
     render() {
         return (
-            <div>
+            <div class="bg-white">
                 <Link to='/'>
-                    <img src={logo} class="m-auto w-48 h-48"/>
+                    <img src={logo} class="m-auto w-48 h-48 fixed inset-x-0"/>
                     </Link>
                 <div class="flex justify-around">
                     <Link to='/'> 
-                        <h1 class=""> HOME </h1>
+                        <h1 class="mt-40 fixed"> HOME </h1>
                     </Link>
                     <Link to='/about'> 
-                        <h1 class=""> ABOUT </h1>
+                        <h1 class="mt-40 fixed"> ABOUT </h1>
                     </Link>
                     <Link to='/calendar'> 
-                        <h1 class=""> CALENDAR </h1>
+                        <h1 class="mt-40 fixed"> CALENDAR </h1>
                     </Link>
                     <Link to='/gallery'> 
-                        <h1 class=""> GALLERY </h1>
+                        <h1 class="mt-40 fixed"> GALLERY </h1>
                     </Link>
                     <Link to='/'> 
-                        <h1 class=""> CONTACT </h1>
+                        <h1 class="mt-40 fixed"> CONTACT </h1>
                     </Link>
                     <div class="flex">
                         <Link to='/'> 
-                            <h1 class=""> SOCIAL#1 </h1>
+                            <h1 class="mt-40 fixed"> SOCIAL#1 </h1>
                         </Link>
                         <Link to='/'> 
-                            <h1 class=""> SOCIAL#2 </h1>
+                            <h1 class="mt-40 fixed"> SOCIAL#2 </h1>
                         </Link>
                         <Link to='/'> 
-                            <h1 class=""> SOCIAL#3 </h1>
+                            <h1 class="mt-40 fixed"> SOCIAL#3 </h1>
                         </Link>
                     </div>
                 </div>
