6d078b08d5bf749d6e1e10559bbfc2fef59dddad
katterillo <kattrina.erillo@gmail.com>
2021-03-14 12:55:12 -0400

    set up executive board page

---
+++ client/src/pages/ExecBoard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class ExecBoard extends Component {
+    render() {
+        return (
+            <div>
+
+                <p>Executive board goes here </p>
+
+            </div>
+                        
+        )
+    }
+}
+
+export default ExecBoard
