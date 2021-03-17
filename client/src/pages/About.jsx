f59604278fa5093d921322a1fcf80cdeb7371d17
katterillo <kattrina.erillo@gmail.com>
2021-03-17 18:43:53 -0400

    made about page responsive

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,80 +7,61 @@ import prehealth from '../images/prehealth.png'
 class About extends Component {
     render() {
         return (
-            <div class="bg-gray-400">
-                <div class="border-r-2 bg-white border-l-2 border-b-2 p-10 m-auto w-7/12">
+            <div class="xl:bg-gray-400">
+                <div class="xl:border-r-2 xl:bg-white xl:border-l-2 xl:border-b-2 p-10 m-auto xl:w-8/12">
                     <div class="m-4">
-                        <div class="border-8 border-black text-center text-5xl p-6 w-10/12 m-auto font-bold">
-                            <h2> WHO THE HEAL ARE WE </h2>
+                        <div class="border-8 border-black text-center text-5xl p-6 w-9/12 xl:w-10/12 m-auto font-bold">
+                            <h2> WHO WE ARE </h2>
                         </div>
                     </div>
-                    <div class="flex flex-row pl-10 mt-20">
-                        <img src={service} alt="Service" class="w-64 rounded border border-blue-300"/>
-                        <div class="flex flex-col ml-auto text-right p-8">
-                            <h3 class="font-semibold"> Service </h3>
-                            <div class="ml-10">
-                                <p class=""> "Lorem ipsum dolor sit amet, 
-                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                Section 1.10.32 of "d
-                                </p>
+                    <div class="">
+                        <div class="flex flex-col text-center xl:text-left xl:flex xl:flex-row mt-20 px-20">
+                            <div class="lg:px-32 xl:px-0 xl:ml-auto xl:float-right">
+                                <h3 class="font-bold text-black text-lg"> Leadership </h3>
+                                <div class="my-5 xl:mt-5 xl:pr-8">
+                                    <p class=""> Leadership sample description: HEAL offers members a variety of ways to
+                                    grow professionally through our research, volunteer, and shadowing opportunities.
+                                    </p>
+                                </div>
                             </div>
+                                <img src={leadership} alt="leadership" class="m-auto w-96 xl:h-64"/>
                         </div>
-                    </div>
-                    <div class="flex flex-row pr-10 mt-20">
-                        <div class="flex flex-col ml-auto px-10">
-                            <h3 class="font-semibold"> Leadership </h3>
-                            <div class="">
-                                <p class=""> "Lorem ipsum dolor sit amet, 
-                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                Section 1.10.32 of "d
-                                </p>
+                        
+                        <div class="flex flex-col xl:flex xl:flex-row-reverse mt-20 px-20">
+                            <div class="lg:px-32 xl:px-0 text-center flex flex-col ml-auto text-left pl-8 xl:text-right">
+                                <h3 class="text-center font-bold text-black text-lg xl:text-right"> Pre-Health </h3>
+                                <div class="my-5 xl:mt-5">
+                                    <p class=""> Pre-health sample description: We provide many resources to our members such as
+                                        informational events with knowledgable speakers and
+                                    educational meetings that encompass all aspects of the healthcare field.
+                                    </p>
+                                </div>
                             </div>
+                            <img src={prehealth} alt="prehealth" class="m-auto w-96 xl:h-64 xl:float-left"/>
                         </div>
-                        <img src={leadership} alt="leadership" class="w-80 h-auto rounded border border-blue-300"/>
-                    </div>
-                    <div class="flex flex-row pl-10 mt-20">
-                        <img src={prehealth} alt="prehealth" class="w-64 rounded border border-blue-300"/>
-                        <div class="flex flex-col ml-auto text-right p-8">
-                            <h3 class="font-semibold"> Pre-Health </h3>
-                            <div class="ml-10">
-                                <p class="">"Lorem ipsum dolor sit amet, 
-                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                Section 1.10.32 of "d
-                                </p>
+                        <div class="flex flex-col text-center xl:text-left xl:flex xl:flex-row mt-20 px-20">
+                            <div class="lg:px-32 xl:px-0 ml-auto xl:float-right">
+                                <h3 class="font-bold text-black text-lg"> Service </h3>
+                                <div class="my-5 xl:mt-5 xl:pr-8">
+                                    <p class=""> Service sample description: Service is a crucial component of HEAL. We volunteer with 
+                                    various non-profit organizations such as Learn to Be, UPchieve, Paper Airplanes, and many more. 
+                                    </p>
+                                </div>
                             </div>
+                            <img src={service} alt="service" class="w-4/12 xl:w-56 m-auto"/>
                         </div>
-                    </div>
-                    <div class="flex flex-row pr-10 mt-20">
-                        <div class="flex flex-col ml-auto px-10">
-                            <h3 class="font-semibold"> Ohana </h3>
-                            <div class="">
-                                <p class=""> Paragraph about service goes here "Lorem ipsum dolor sit amet, 
-                                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                Section 1.10.32 of "d
-                                </p>
+                        <div class="flex flex-col text-center xl:flex xl:flex-row-reverse mt-20 px-20">
+                            <div class="lg:px-32 xl:px-0 flex flex-col ml-auto pl-8 xl:text-right">
+                                <h3 class="font-bold text-black text-lg"> Ohana </h3>
+                                <div class="my-5 xl:mt-5">
+                                    <p class=""> Ohana sample description: HEAL fosters a family atmosphere and offers social events 
+                                    for our members to build strong relationships that will last throughout college and 
+                                    into the professional world.
+                                    </p>
+                                </div>
                             </div>
+                            <img src={ohana} alt="Service" class="w-96 m-auto xl:h-64"/>
                         </div>
-                        <img src={ohana} alt="ohana" class="w-64 rounded border border-blue-300"/>
                     </div>
                 </div>
             </div>
