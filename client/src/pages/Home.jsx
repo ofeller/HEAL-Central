cf2a56c9d9968e203f8a2603932897c4d83077c4
Olivia Feller <oliviafeller@Olivias-MacBook-Pro-2.local>
2021-03-18 16:06:25 -0400

    Basic fixed banner, image/text not styled properly yet

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,8 +3,22 @@ import React, { Component } from 'react'
 class Home extends Component {
     render() {
         return (
+        
             <div>
-                <img src = "samplebanner.jpeg" alt="sample banner" class="w-48 h-48"/>
+                <div class="bg-gray-400">
+                <div class="p-28 mb-4 bg-banner opacity-75 bg-cover text-center bg-fixed bg-right">  
+                     <div class="bg-black bg-opacity-50 text-10xl tracking-widest text-white font-medium opacity-100">
+                         HEAL
+                     </div>
+                     <div class="text-3xl italic tracking-widest text-white">
+                         Health Educated Asian Leaders
+                     </div>
+                     <div class="text-2xl font-medium tracking-wider text-black">
+                         University of Florida
+                     </div>
+                </div>
+                </div>
+                
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,29 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'samplebanner': "url('images/samplebanner.jpeg')",
+        'banner': "url('images/heal-banner.jpeg')",
+       })
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '3rem',
+       '6xl': '3.75rem',
+      '7xl': '4.5rem',
+      '8xl': '6rem',
+      '9xl': '8rem',
+      '10xl': '12rem',
+
+     },
   },
   variants: {
     extend: {},
