5a323b633f8fe8af51e2dcde54881cbc11aaf3eb
Dongni Alcantara <dogni31@hotmail.com>
2021-03-21 14:27:47 -0400

    Made gallery into a grid of 3 columns

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -1,15 +1,47 @@
 import React, { Component } from 'react'
+import ohana from '../images/ohana.jpg'
 
 class Gallery extends Component {
     render() {
         return (
-            <body class ="bg-blue-200">
+            <div class="bg-contain bg-gradient-to-r from-blue-100 via-blue-200 to-purple-300 ... h-screen">
+                <div class=" text-5xl p-6 w-10/12 mb-4 m-auto font-bold">
+                    <h2 class="italic tracking-wider "> GALLERY </h2>
+                </div>
+                <div class="border-2 border-green-300 border-opacity-75 grid grid-cols-3 gap-2 place-content-center h-30">
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+                    <div>
+                        <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
+                    </div>
+
+                </div>
+
 
             
-            <div>
-                <h1>Gallery Page</h1>
             </div>
-            </body>
         )
     }
 }
