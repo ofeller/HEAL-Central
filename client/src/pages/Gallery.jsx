3b2473e2be5a06ee76950d5e3d563714864c1869
Dongni Alcantara <dogni31@hotmail.com>
2021-03-21 15:41:08 -0400

    Fixed up the page to be centered and for background to extend to whole page

--- client/src/pages/Gallery.jsx
+++ client/src/pages/Gallery.jsx
@@ -4,11 +4,11 @@ import ohana from '../images/ohana.jpg'
 class Gallery extends Component {
     render() {
         return (
-            <div class="bg-contain bg-gradient-to-r from-blue-100 via-blue-200 to-purple-300 ... h-screen">
+            <div class="bg-contain bg-gradient-to-r from-blue-100 via-blue-200 to-purple-300 ... h-auto p-28">
                 <div class=" text-5xl p-6 w-10/12 mb-4 m-auto font-bold">
                     <h2 class="italic tracking-wider "> GALLERY </h2>
                 </div>
-                <div class="border-2 border-green-300 border-opacity-75 grid grid-cols-3 gap-2 place-content-center h-30">
+                <div class="grid grid-cols-3 gap-14 place-content-center px-8">
                     <div>
                         <img src={ohana} alt="ohana" class="m-auto w-96 xl:h-64"/>
                     </div>
