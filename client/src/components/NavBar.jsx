4a82f89ed78e3122adc761a6b371f1f1cef62065
katterillo <kattrina.erillo@gmail.com>
2021-03-21 13:11:52 -0400

    made NavBar responsive

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -14,30 +14,30 @@ class NavBar extends Component {
                         <img src={logo} class="m-auto w-20 h-20 my-1"/>
                     </Link>
                     <Link to='/'> 
-                        <h1 class=""> HOME </h1>
+                        <h1 class="hover:text-teal"> HOME </h1>
                     </Link>
                     <Link to='/about'> 
-                        <h1 class=""> ABOUT </h1>
+                        <h1 class="hover:text-teal"> ABOUT </h1>
                     </Link>
                     <Link to='/calendar'> 
-                        <h1 class=""> CALENDAR </h1>
+                        <h1 class="hover:text-teal"> CALENDAR </h1>
                     </Link>
                     <Link to='/gallery'> 
-                        <h1 class=""> GALLERY </h1>
+                        <h1 class="hover:text-teal"> GALLERY </h1>
                     </Link>
                     <Link to='/execboard'> 
-                        <h1 class=""> EXECUTIVE BOARD </h1>
+                        <h1 class="hover:text-teal"> EXECUTIVE BOARD </h1>
                     </Link>
                     <Link to='/'> 
-                        <h1 class=""> CONTACT </h1>
+                        <h1 class="hover:text-teal"> CONTACT </h1>
                     </Link>
                     <div class="flex items-center">
                         <a href='https://www.instagram.com/ufheal/' target="_blank"> 
-                        <img src= {instagram} class="m-auto w-8 h-8 my-1 mx-2"/>
+                        <img src= {instagram} class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 m-auto w-8 h-8 my-1 mx-2"/>
                         </a>
                
                         <a href='https://www.facebook.com/healatuf' target="_blank"> 
-                        <img src= {facebook} class="m-auto w-8 h-8 my-1 mx-2"/>
+                        <img src= {facebook} class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 m-auto w-8 h-8 my-1 mx-2"/>
                         </a>
            
                         <Link to='/'> 
