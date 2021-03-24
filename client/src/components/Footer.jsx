19284e2b5576f5b2832fb3214d2a38336fd1ccd8
katterillo <kattrina.erillo@gmail.com>
2021-03-24 14:28:41 -0400

    added social media to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -10,8 +10,20 @@ class Footer extends Component {
     render() {
         return (
             <React.Fragment>
-            <div class="overflow-visible mt-5 bottom-0 text-center">
-                <h2 class="font-light"> HEALTH EDUCATED ASIAN LEADERS | UNIVERSITY OF FLORIDA</h2>
+            <div id="Footer" class="bg-mint-green overflow-visible bottom-0 text-center py-2">
+                <h2 class="font-light"> 
+                HEALTH EDUCATED ASIAN LEADERS | UNIVERSITY OF FLORIDA
+                </h2>
+                <h3 class="font-light m-4"> Stay Connected: </h3>
+                <div class="flex justify-center m-2">
+                        <a href='https://www.instagram.com/ufheal/' target="_blank"> 
+                        <img src= {instagram} class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 m-auto w-8 h-8 my-1 mx-2"/>
+                        </a>
+               
+                        <a href='https://www.facebook.com/healatuf' target="_blank"> 
+                        <img src= {facebook} class="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 m-auto w-8 h-8 my-1 mx-2"/>
+                        </a>
+                    </div>
                 <BackToTop/>
             </div>
             </React.Fragment>
