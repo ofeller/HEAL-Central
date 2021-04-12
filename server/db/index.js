627ba54f660ba1a67caf68dd693b80310e76c733
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 12:30:40 -0400

    removed mongodb from connection link

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://mongodb:27017/signup', { useNewUrlParser: true })
+    .connect('mongodb://127.0.0.1:27017/signup', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
