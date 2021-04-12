b8f219d3caad63fb3d5fa97acca22bebc11aa74d
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 11:11:38 -0400

    check error

--- server/index.js
+++ server/index.js
@@ -13,55 +13,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.json())
 
-// Specification for http server
-var http = require('http'), 
-	fs = require('fs'), 
- 	url = require('url');
-
-/* Global variables */
-var listingData, server;
-
-var requestHandler = function(request, response) {
-  	var parsedUrl = url.parse(request.url);
-
-  	/*
-    	This request handler should send listingData in the JSON format if a GET request 
-    	is sent to the '/listings' path. Otherwise, it should send a 404 error.
-    
-   */
-   
-   if (parsedUrl.pathname == '/listings') {
-  		response.write(listingData);
-  		response.end();
-  	} 
-  	
-  	else {
-  		response.writeHead(404, {"Content-Type": "text/plain"});
-  		response.write('404, Page Not Found');
-  		response.end();
-  	}
-};
-
-// Server initialization
-var server = http.createServer(requestHandler);
-
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  	/*
-   	This callback function should save the data in the listingData variable, 
-    	then start the server. 
-   */
-   // error and data handling
-  	if(err) throw err;
-  
-  	listingData = data;
-
-	// Start the server
-  	server.listen(apiPort, function() {
-		console.log('Server listening on: http://127.0.0.1:' + apiPort);   
-	});
-   
-});
-
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
@@ -71,8 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', attendeeRouter)
 
-
-/*if(!module.parent){
+if(!module.parent){
     app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
-}*/
+}
 module.exports = app.listen(3000);
