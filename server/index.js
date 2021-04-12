a493414f5677a998aa01d7fd64a0c75789f92805
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 13:07:13 -0400

    25-27 commented

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 
-//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', attendeeRouter)
 
-if(!module.parent){
+/*if(!module.parent){
     app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
-}
+}*/
 module.exports = app.listen(3001);
