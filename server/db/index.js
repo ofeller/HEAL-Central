acb1e18676c5d6074eb6ac5c0b65242573cb5e92
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 12:09:10 -0400

    kill

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
-console.log(`Reached here 1`)
+
 mongoose
     .connect('mongodb://127.0.0.1:27017/signup', { useNewUrlParser: true })
     .catch(e => {
-        console.log(`Reached here 2`)
         console.error('Connection error', e.message)
     })
 
