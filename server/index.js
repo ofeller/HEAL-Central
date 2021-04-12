8012c912d8bea36f2c476e0709d12bc9cd84be8c
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 13:05:55 -0400

    comment line 17

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+//db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
