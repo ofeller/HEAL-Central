7809a41d67a54fa7683d216cfd9ea07e4b653f37
kattrina.erillo <kattrina.erillo@gmail.com>
2021-04-12 20:08:12 -0400

    connected to atlas mongodb password in .env

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
+require('dotenv').config();
+var url = process.env.MONGODB_URI;
+
+
 mongoose
-    .connect('mongodb://127.0.0.1:27017/signup', { useNewUrlParser: true })
+    .connect(url || 'mongodb://127.0.0.1:27017/signup', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+
 const db = require('./db')
 const attendeeRouter = require('./routes/attendee-router')
 
