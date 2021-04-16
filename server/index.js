a3f0333579ed571fe39fe55669e8a95519756ddd
Olivia Feller <oliviafeller@Olivias-MacBook-Pro-2.local>
2021-04-16 14:47:52 -0400

    updating index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 
 require('dotenv').config({ path: require('find-config')('.env') })
 const db = require('./db')
